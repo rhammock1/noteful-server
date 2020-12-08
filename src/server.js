59b1f6c301c47fcb0b2a4e1c39e39a94ccf1a7fa
rhammock1 <rhamm1027@gmail.com>
2020-12-08 11:35:09 -0500

    setup to start work on server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const app = require('./app');
-const {PORT} = require('./config');
+const knex = require('knex');
+const {PORT, DB_URL} = require('./config');
 const MODE = process.env.NODE_ENV;
+
+
+const db = knex({
+  client: 'pg',
+  connection:DB_URL,
+})
+
+app.set('db', db);
 
 app.listen(PORT, () => {
   console.log(`Server running in ${MODE} mode and listening at port: ${PORT}`)
