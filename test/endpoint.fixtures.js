858a12ccc920062314bc68905d158fee803eeae8
rhammock1 <rhamm1027@gmail.com>
2020-12-08 12:31:52 -0500

    added GET test with test articles from fixture

---
+++ test/endpoint.fixtures.js
@@ -0,0 +1,10 @@
+function makeFoldersArray() {
+    return [
+    { id: 1, folder_name: 'Test1' },
+    { id: 2, folder_name: 'Test2'},
+    { id: 3, folder_name: 'Test3'}
+  ];
+}
+module.exports = {
+  makeFoldersArray,
+}
--- test/folders-endpoints.test.js
+++ test/folders-endpoints.test.js
@@ -2,10 +2,11 @@ const { expect } = require('chai');
 const knex = require('knex');
 const supertest = require('supertest');
 const app = require('../src/app');
+const { makeFoldersArray } = require('./endpoint.fixtures');
 
 describe('Folders Endpoints', function() {
   let db;
-  let testFolders = [];
+
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
@@ -15,20 +16,57 @@ describe('Folders Endpoints', function() {
   })
 
   after('disconnect from db', () => db.destroy())
-  before('clean the table', () => {
-    db('noteful_folders', 'notefule_notes').truncate();
-  })
-  afterEach('Cleanup', () => {
-    db('noteful_folders', 'notefule_notes').truncate()
-  })
+  before('clean the table', () => 
+    db.raw('TRUNCATE noteful_notes, noteful_folders RESTART IDENTITY CASCADE')
+  )
+  afterEach('Cleanup', () => 
+    db.raw('TRUNCATE noteful_notes, noteful_folders RESTART IDENTITY CASCADE')
+  )
 
   describe('GET /api/folders', () => {
     context('Given there are no folders', () => {
-      it('responds wiht 200 and an empty list', () => {
+      it('responds with 200 and an empty list', () => {
         return supertest(app)
           .get('/api/folders')
           .expect(200, [])
       })
     })
+    context('Given there are folders in the database', () => {
+      const testFolders = makeFoldersArray();
+
+      beforeEach('insert folders', () => {
+        return db
+          .into('noteful_folders')
+          .insert(testFolders)
+      })
+      it('GET /api/folders responds with 200 and all of the folders', () => {
+        return supertest(app)
+          .get('/api/folders')
+          .expect(200, testFolders)
+      })
+    })
   })
 })
+
+    // context(`Given an XSS attack folder`, () => {
+    //  const maliciousFolder = {
+    //    id: 911,
+    //    folder_name: 'Naughty naughty very naughty <script>alert("xss");</script>',
+    //  }
+
+    //  beforeEach('insert malicious folder', () => {
+    //    return db
+    //      .into('noteful_folders')
+    //      .insert([ maliciousFolder ])
+    //  })
+
+    //  it('removes XSS attack content', () => {
+    //    return supertest(app)
+    //      .get(`/api/folders/${maliciousFolder.id}`)
+    //      .expect(200)
+    //      .expect(res => {
+    //        expect(res.body.folder_name).to.eql('Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;')
+           
+    //      })
+    //  })
+    // })
