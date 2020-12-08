c65bc8db7f2b77030a4b33d01b0504183abce9b4
rhammock1 <rhamm1027@gmail.com>
2020-12-08 14:38:45 -0500

    Added tests for DELETE enpoint

--- test/folders-endpoints.test.js
+++ test/folders-endpoints.test.js
@@ -163,5 +163,35 @@ describe('Folders Endpoints', function() {
       })
     })
   })
+  describe.only('DELETE /api/folders/:folderId', () => {
+    context('Given no folders', () => {
+      it('responds with 404', () => {
+        const folderId = 123456;
+        return supertest(app)
+          .delete(`/api/folders/${folderId}`)
+          .expect(404, { error: { message: `Article doesn't exist` } })
+      })
+    })
+    context('Given there are folders in the database', () => {
+      const testFolders = makeFoldersArray();
+      beforeEach('insert folders', () => {
+        return db
+          .into('noteful_folders')
+          .insert(testFolders)
+      })
+      it('responds with 204 and removes the folder', () => {
+        const idToRemove = 2;
+        const expectedFolder = testFolders.filter(folder => folder.id !== idToRemove)
+        return supertest(app)
+          .delete(`/api/folders/${idToRemove}`)
+          .expect(204)
+          .then(res => 
+            supertest(app)
+              .get(`/api/folders`)
+              .expect(expectedFolder)
+            )
+      })
+    })
+  })
 })
 
