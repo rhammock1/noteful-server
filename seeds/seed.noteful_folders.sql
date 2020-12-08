9acd51f632e5f24668a78d011471f333cff87e65
rhammock1 <rhamm1027@gmail.com>
2020-12-08 10:03:30 -0500

    Created seed test data for notes-edited folders

--- seeds/seed.noteful_folders.sql
+++ seeds/seed.noteful_folders.sql
@@ -1,3 +1,4 @@
 INSERT INTO noteful_folders (folder_name)
 VALUES
 ('Important'), ('Ideas'), ('Random-Things');
+--id 1            2             3
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,11 @@
+INSERT INTO noteful_notes (title, content, folder_id)
+VALUES
+('Test Title', 'Test content', 1),
+('Test Title1', 'Test content8', 2),
+('Test Title2', 'Test content7', 3),
+('Test Title3', 'Test content6', 1),
+('Test Title4', 'Test content5', 2),
+('Test Title5', 'Test content4', 3),
+('Test Title6', 'Test content3', 1),
+('Test Title7', 'Test content2', 2),
+('Test Title8', 'Test content1', 3);
