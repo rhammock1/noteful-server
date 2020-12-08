6051f758738c31e30026bde9996651c9a2db0c8e
rhammock1 <rhamm1027@gmail.com>
2020-12-08 16:21:00 -0500

    Added GET /:noteId

--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -48,4 +48,23 @@ notesRouter
       .catch(next)
   })
 
+  notesRouter
+    .route('/:noteId')
+    .all((req, res, next) => {
+        const db = req.app.get('db');
+    notesService.getById(db, req.params.noteId)
+      .then(note => {
+        if(!note) {
+          return res.status(404).json({
+            error: { message: `Note doesn't exist` }
+          })
+        }
+        res.note = note;
+        next();
+      })
+      .catch(next)
+    })
+    .get((req, res, next) => {
+      res.status(200).json(serializeNote(res.note))
+    })
   module.exports = notesRouter
